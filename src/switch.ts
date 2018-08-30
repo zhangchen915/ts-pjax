f78b325336cc4de1d41f50453f63244e78a2301f
zhangchen915 <zhangchen915@outlook.com>
2018-08-30 18:03:29 +0800

    add: switchOptions

--- src/switch.ts
+++ src/switch.ts
@@ -1,4 +1,15 @@
-import { on } from './even'
+interface switchOptions {
+    classNames?: {
+        add: string,
+        remove: string,
+        forward: string,
+        backward: string
+    }
+    callbacks?: {
+        addElement: Function,
+        removeElement: Function
+    }
+}
 
 export default {
     outerHTML: function (oldEl, newEl) {
@@ -7,13 +18,7 @@ export default {
 
     innerHTML: function (oldEl, newEl) {
         oldEl.innerHTML = newEl.innerHTML
-
-        if (newEl.className === "") {
-            oldEl.removeAttribute("class")
-        }
-        else {
-            oldEl.className = newEl.className
-        }
+        oldEl.className = newEl.className
     },
 
     switchElementsAlt: function (oldEl, newEl) {
@@ -21,9 +26,8 @@ export default {
 
         // Copy attributes from the new element to the old one
         if (newEl.hasAttributes()) {
-            var attrs = newEl.attributes
-            for (var i = 0; i < attrs.length; i++) {
-                oldEl.attributes.setNamedItem(attrs[i].cloneNode())
+            for (let i = 0; i < newEl.attributes.length; i++) {
+                oldEl.attributes.setNamedItem(newEl.attributes[i].cloneNode())
             }
         }
     },
@@ -33,43 +37,33 @@ export default {
         oldEl.parentNode.replaceChild(newEl, oldEl);
     },
 
-    sideBySide: function (oldEl, newEl, options, switchOptions) {
-        var forEach = Array.prototype.forEach
+    sideBySide: function (oldEl, newEl, options, switchOptions: switchOptions = {}) {
         var elsToRemove = []
         var elsToAdd = []
-        var fragToAppend = document.createDocumentFragment()
-        var animationEventNames = "animationend webkitAnimationEnd MSAnimationEnd oanimationend"
-        var animatedElsNumber = 0
-        var sexyAnimationEnd = function (e) {
-            if (e.target !== e.currentTarget) {
-                // end triggered by an animation on a child
-                return
-            }
+        var animatedElsNumber = 0;
+        const fragToAppend = document.createDocumentFragment()
+        const animationEventNames = "animationend webkitAnimationEnd MSAnimationEnd oanimationend"
+        const sexyAnimationEnd = e => {
+            // end triggered by an animation on a child
+            if (e.target !== e.currentTarget) return;
 
             animatedElsNumber--
             if (animatedElsNumber <= 0 && elsToRemove) {
-                elsToRemove.forEach(function (el) {
-                    // browsing quickly can make the el
-                    // already removed by last page update ?
-                    if (el.parentNode) {
-                        el.parentNode.removeChild(el)
-                    }
+                elsToRemove.forEach(el => {
+                    if (el.parentNode) el.parentNode.removeChild(el)
                 })
 
-                elsToAdd.forEach(function (el) {
+                elsToAdd.forEach(el => {
                     el.className = el.className.replace(el.getAttribute("data-pjax-classes"), "")
                     el.removeAttribute("data-pjax-classes")
                 })
 
-                elsToAdd = null // free memory
-                elsToRemove = null // free memory
-
+                elsToAdd = null
+                elsToRemove = null
             }
         }
 
-        switchOptions = switchOptions || {}
-
-        forEach.call(oldEl.childNodes, function (el) {
+        Array.from(oldEl.childNodes).forEach((el: Element) => {
             elsToRemove.push(el)
             if (el.classList && !el.classList.contains("js-Pjax-remove")) {
                 // for fast switch, clean element that just have been added, & not cleaned yet.
@@ -78,32 +72,28 @@ export default {
                     el.removeAttribute("data-pjax-classes")
                 }
                 el.classList.add("js-Pjax-remove")
-                if (switchOptions.callbacks && switchOptions.callbacks.removeElement) {
-                    switchOptions.callbacks.removeElement(el)
-                }
+                if (switchOptions.callbacks && switchOptions.callbacks.removeElement) switchOptions.callbacks.removeElement(el);
                 if (switchOptions.classNames) {
-                    el.className += " " + switchOptions.classNames.remove + " " + (options.backward ? switchOptions.classNames.backward : switchOptions.classNames.forward)
+                    el.classList.add(switchOptions.classNames.remove)
+                    el.classList.add(options.backward ? switchOptions.classNames.backward : switchOptions.classNames.forward)
                 }
                 animatedElsNumber++
-                on(el, animationEventNames, sexyAnimationEnd, true)
+                el.addEventListener(animationEventNames, sexyAnimationEnd, true)
             }
         })
 
-        forEach.call(newEl.childNodes, function (el) {
+        Array.from(newEl.childNodes).forEach((el: Element) => {
             if (el.classList) {
-                var addClasses = ""
                 if (switchOptions.classNames) {
-                    addClasses = " js-Pjax-add " + switchOptions.classNames.add + " " + (options.backward ? switchOptions.classNames.forward : switchOptions.classNames.backward)
+                    el.classList.add(switchOptions.classNames.add);
+                    el.classList.add(options.backward ? switchOptions.classNames.forward : switchOptions.classNames.backward);
                 }
-                if (switchOptions.callbacks && switchOptions.callbacks.addElement) {
-                    switchOptions.callbacks.addElement(el)
-                }
-                el.className += addClasses
-                el.setAttribute("data-pjax-classes", addClasses)
+                if (switchOptions.callbacks && switchOptions.callbacks.addElement) switchOptions.callbacks.addElement(el);
+
                 elsToAdd.push(el)
                 fragToAppend.appendChild(el)
-                animatedElsNumber++
-                on(el, animationEventNames, sexyAnimationEnd, true)
+                animatedElsNumber++;
+                el.addEventListener(animationEventNames, sexyAnimationEnd, true)
             }
         })
 
